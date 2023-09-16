fc46f9f566276da77b18a8cee59a0e66e365fd01
Tyler <tyler001crawford@gmail.com>
2023-09-16 19:40:21 -0400

    register and login user php files added

---
+++ PHP/loginUser.php
@@ -0,0 +1,68 @@
+
+<?php
+
+	$inData = getRequestInfo();
+	
+    $servername = "localhost";
+    $ServerUsername = "TO be inserted";
+    $ServerPassword = "TO be inserted";
+    $dbname = "TO be inserted";
+
+	$conn = new mysqli($servername, $ServerUsername, $ServerPassword, $dbname);
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $username = $inData['login'];
+        $password = $inData['password'];
+
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $username);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+            if (password_verify($password, $row['Password'])) 
+            {
+                returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+            } 
+            else 
+            {
+                returnWithError("Invalid Username or Password");
+            }
+        }
+		else
+		{
+			returnWithError("Invalid Username or Password");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
