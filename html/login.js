5a532cfd81d0dfd6cb3386f3310a85b1478cdef8
Tyler <tyler001crawford@gmail.com>
2023-09-23 21:17:08 -0400

    login php integration + revisions to register.html

--- html/login.js
+++ html/login.js
@@ -1,35 +1,36 @@
 // ALL OF THIS IS AJAX ENABLED
-// redirects REQUIRED HERE
+const registerButton = document.getElementById("register-button");
+// Add a click event listener to the button
+registerButton.addEventListener("click", function () {
+  // Redirect to register.html
+  window.location.href = "register.html";
+});
+
 document.addEventListener("DOMContentLoaded", function () {
     // Get a reference to the form
-    const form = document.getElementById("registration-form");
+    const form = document.getElementById("login-form");
     // Add a submit event listener to the form
     form.addEventListener("submit", function (event) 
     {
         event.preventDefault(); // Prevent the default form submission behavior
 
         // Get input values
-        const firstName = document.getElementById("firstName").value;
-        const lastName = document.getElementById("lastName").value;
-        const login = document.getElementById("login").value;
+        const login = document.getElementById("username").value;
         const password = document.getElementById("password").value;
-        const confirmPassword = document.getElementById("confirm_password").value;
 
         // Create a JavaScript object
         const userData = {
-            firstName: firstName,
-            lastName: lastName,
             login: login,
             password: password,
-            confirmPassword: confirmPassword,
         };
 
         // Convert the JavaScript object to JSON
         const jsonData = JSON.stringify(userData);
+
         // Create a new XMLHttpRequest or use the fetch API to send the JSON data to your PHP script
         // Here's an example using XMLHttpRequest:
         const xhr = new XMLHttpRequest();
-        xhr.open("POST", "PHP/registerUser.php", true);
+        xhr.open("POST", "PHP/loginUser.php", true);
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -50,9 +51,6 @@ document.addEventListener("DOMContentLoaded", function () {
                   console.log("ID:", response.id);
                   console.log("First Name:", response.firstName);
                   console.log("Last Name:", response.lastName);
-                  console.log("Username:", response.login);
-                  //Successful redirect would go here
-
                 }
               } 
               else {
@@ -60,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error("Registration failed.");
                 //formatting for the error message on an invalid registration that will appear on the user's end
                 const errorMessage = document.getElementById("error-message");
-                errorMessage.textContent = "Registration failed. The username might already exist or the passwords do not match.";
+                errorMessage.textContent = "Username or Password is Invalid";
                 errorMessage.style.color = "red"; // Set the text color to red or customize it as needed
               }
             }
